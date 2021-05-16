a629245a4b1ef96abda9fa7a6a9b2c9141243e0e
tomizawa0214 <j.tomizawa.japan@gmail.com>
2021-05-16 16:17:34 +0900

    localhost

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
 const Index = () => {
-  <div>
-    <p>Sample app using React and Next.js</p>
-  </div>
+  return(
+    <div>
+      <p>Sample app using React and Next.js</p>
+    </div>
+  )
 };
 
 export default Index;
